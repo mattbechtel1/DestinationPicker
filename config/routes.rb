798db8c50d856430c79cc0eb407052b4a381c344
Matthew Bechtel <bechma02@gmail.com>
2024-05-28 21:12:09 -0700

    Add home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "destinations#home"
 
   get "/destinations", to: "destinations#index"
   get "/select", to: "destinations#random"
